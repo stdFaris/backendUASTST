bf6dbbedf1eb0cfdd85ce7d8e86945d6aa7a1fdf
stdFaris <18222029@std.stei.itb.ac.id>
2025-05-17 18:03:52 +0700

    Update main.py

--- main.py
+++ main.py
@@ -14,10 +14,6 @@ app = FastAPI()
 
 # Konfigurasi CORS
 origins = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-    "http://localhost:5174",
-    "http://127.0.0.1:5174",
     "https://santairumah.netlify.app",
 ]
 
