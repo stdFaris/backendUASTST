a73476079ad0c3bbb923c09d84e4146c3458e53a
stdFaris <18222029@std.stei.itb.ac.id>
2025-05-26 01:48:44 +0700

    Update main.py

--- main.py
+++ main.py
@@ -19,9 +19,9 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allow_headers=["*"],
 )
 
